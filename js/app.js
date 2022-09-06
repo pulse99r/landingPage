5642fc1b23b9cd9c5183f19ee0270c141d7eacc8
Kenti Johnson <97370716+pulse99r@users.noreply.github.com>
2022-09-06 08:37:01 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,153 +1,111 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
 
+//Global variables for the sections and the nav unordered list tag
+const theSections = document.getElementsByTagName('section'); //document sections
+const navUl = document.getElementById("navbar__list"); //UL nav tag
 
-/**
- * Define Global Variables
- * 
-*/
-/**  Get the unordered list from the page            */
-const navbarList = document.getElementById('navbar__list');
+const sectionTops = []; //captures the positions of the tops of the sectiions
+const sectionBottoms = []; //captures the positions of the bottoms of sections
+let currrentSectionInView = ''; //section currently in viewport
 
-/* Gets the sections from the page                        */
-const theSections = document.querySelectorAll('section');
-const theSectionDivs = document.querySelectorAll('.landing__container');
-/** 
-    Captures the number of sections from length property 
-    of the sections to be used as a counter in the loop 
-    to create the navbar list items.                          */
-const sectionCount = theSections.length; 
+function sectionPositions() {
+    const footerPos = document.getElementById('footer').getBoundingClientRect().top + window.pageYOffset;
 
-/*
-    Captures the viewport height for use in determining  
-    the position of the sections during scroll                 */
-// const vpHeight = window.innerHeight;
+    for(let i = 0; i < theSections.length; i++) {
+        //Tops of each section
+        let theTops = theSections[i].getBoundingClientRect().top + window.pageYOffset;
+        let theBottoms = theSections[i].getBoundingClientRect().bottom + window.pageYOffset;
+        sectionTops[i] = theTops;
+        sectionBottoms[i] = theBottoms
+        if (i + 1 === theSections.length) { //This adds the top of the footer to the array, to help with checking the bottom of the last section. 
+            sectionTops[i + 1] = footerPos;
+        }
+    }
+    console.log("*** SectionPositions: sectionTops: ", sectionTops);
+}
 
-/**
- * End Global Variables
- *
- *  Start Helper Functions
- * 
-*/
-
-/* Create the list items for navbarList                   */
-
-const navItems = (sections) => {
-    let anchorTag = "";
-    for (let i=0; i < sectionCount; i++) {
-        /* Create the navigation list items          */
-        const listItem = document.createElement('li');
-        anchorTag = document.createElement('a');
-        anchorTag.setAttribute("href", '#'+ sections[i].id);
-        anchorTag.textContent = sections[i].dataset.nav;
-        anchorTag.classList.add("menu__link")
-        listItem.appendChild(anchorTag);
-        navbarList.appendChild(listItem);
-        anchorTag.addEventListener("click", (event) => {
-            /* 
-              The function scrollToSection performs the scroll to
-              the section clicked                                    */
-              vpHeight = window.innerHeight;
-              scrollToSection(sections[i]);
-
-            /*
-              The remaining code in this anchorTag event listener
-              highlight the clicked navigation tab by setting 
-              it's background color to black and the text color to white */
-             
-            /*  get the nav anchor tag elements                         */
-            const navElemAs = document.getElementsByTagName('a');
-            
-            /* gets the event target                                     */
-            const aTagHref = event.target.getAttribute('href');
-            /*
-            Loop through and compare the anchor tag elements 
-              with the clicked target, then set the matching anchor 
-              tag background and text color, by adding the drk__color 
-              class, while removing "drk__color" from any non-matching 
-              anchor tag elements                                     */ 
-            for (let j=0; j < sectionCount; j++) {    
-                if(navElemAs[j].getAttribute("href") === aTagHref){
-                    navElemAs[j].classList.add("drk__color");                    
-                    theSections[i].classList.add("your-active-class");
-                } else {
-                    navElemAs[j].classList.remove("drk__color");
-                    theSections[i].classList.remove("your-active-class");
-                }
-            } 
+//Clears the active 'state flag' for the Navigation Ancor Tags and and the sections
+function clearActiveStates () {
+    const anchorTags = document.getElementsByTagName('a');
+    for(let i = 0; i < theSections.length; i++) {
+        theSections[i].classList.remove('your-active-class');
+        anchorTags[i].classList.remove('active__tag');
+    } 
+}
+/*This function builds out the navigation list items with a tags and appends 
+them to the the navigation ul for the menu. */  
+function buildNavItems (){
+    for(let i=0; i<theSections.length; i++){
+        const listNavItems = document.createElement("li");
+        const aNavTags = document.createElement('a');
+        //get data-nav attribute from sections
+        const aTextLabel = theSections[i].getAttribute('data-nav');
+        aNavTags.innerHTML = aTextLabel;
+        aNavTags.classList.add('menu__link');
+        aNavTags.setAttribute("href","#" + theSections[i].id);
+        aNavTags.setAttribute("data-id", theSections[i].id);
+        listNavItems.append(aNavTags);
+        navUl.append(listNavItems);
+        /**listens for click event, moves to section indicated by 
+         * click nav item updates the active states of the menu and the sections */ 
+        aNavTags.addEventListener('click', function(ev) {
+            ev.preventDefault(); 
+            const clickedATag = ev.target.getAttribute('data-id');
+            const destinationSection = document.getElementById(clickedATag);
+            clearActiveStates();
+            ev.target.classList.add('active__tag');
+            destinationSection.classList.add('your-active-class')
+            destinationSection.scrollIntoView({
+                behavior: 'smooth'
+            });
         });
     }
-};
-
-/*
-  The scrollToSection function accepts one parameter, 'section' and  
-  scrolls to that section of the page. The argument is based on the
-  clicked section of the page.                                      */
-function scrollToSection (section) {
-    section.scrollIntoView({behavior: "smooth"});
 }
 
 
-/* 
-   This statement calls the NavItems function to append the list 
-   items to the unordered list element to create the navigation bar. */
-navItems(theSections);
+/* keeps track of the scrolling position and updates the active states 
+of both the sections in view and the menu a tags.*/
+//handles the scrolling and updates the active tags and sections during scrolling
+function doTheScroll() {
+    /* 1. Get the scroll position (scrollPosition)
+       2. Build a table of the tops of each section
+       3. If the scroll position is betweem the top of Section A 
+          and the top of Section B, then that section is in the 
+          viewport.
+       4. Update the active state for that section
+    */
+    const scrollPosition = window.pageYOffset;
+    console.log("scrollPosition ====>", scrollPosition)
+    const navAncorTags = document.getElementsByTagName('a');
 
-
-/* This event listener listens for scroll events and invokes 
-   the setActiveClass function passing in the section currently in the
-   viewport (currentSection), the position of the top (sectionPosition.top) 
-   and the position of the bottom (sectionPosition.bottom)                  */
-window.addEventListener("scroll", () =>{ 
-    vpHeight = window.innerHeight;
-    for(let j = 0; j <theSections.length; j++) {
-        let currentSection = theSections[j].id;
-        let sectionPosition = theSections[j].getBoundingClientRect();
-        setActiveClass(currentSection,sectionPosition.top, sectionPosition.bottom);
-    }
-})
-
-/* setActiveClass sets the the matching anchor tag background 
-   and text color, by adding the drk__color class, while removing 
-   "drk__color" from any non-matching anchor tag elements while 
-   SCROLLING through the materials                               */
-function setActiveClass (currentSection, topPosition, bottomPosition) {
-    
-    if (topPosition > 0 
-        && topPosition < vpHeight/3
-        && (bottomPosition <= vpHeight || bottomPosition >= vpHeight)){
-        //console.log("section/top/bottom: \n",currentSection, topPosition, bottomPosition);
-        let sectionId = currentSection;
-
-        const navElements = document.getElementsByTagName("a");
+    const vpHeight = window.innerHeight;
+    console.log("viewport height: ", vpHeight, "scrollPosition: ", scrollPosition);
+    for(let i = 0; i < theSections.length; i++){
+        let sectionATop = sectionTops[i];
+        let sectionBTop = sectionTops[i+1];
+        let sectionABottom = sectionBottoms[i]
         
-        for (let i = 0; i<navElements.length; i++) {
-            const navElementHref = navElements[i].getAttribute("href");
-            if(navElementHref === "#"+sectionId) {
-                navElements[i].classList.add("drk__color");
-                theSections[i].classList.add("your-active-class");
-            } else {
-                navElements[i].classList.remove("drk__color");
-                //theSectionDivs[i].classList.remove("landing__container");
-                theSections[i].classList.remove("your-active-class")
-            }
+        if(scrollPosition + 20 >= sectionATop 
+            && scrollPosition <= sectionBTop && sectionATop > vpHeight/6) { 
+            console.log(theSections[i].id,"scrollPosition ",scrollPosition, "is greater than ", 
+            " sectionATop:", sectionATop, "and scrollPosition ",scrollPosition, 
+            "is LESS than sectionBTop", sectionBTop,
+            " and sectionABottom");
+            clearActiveStates();
+            navAncorTags[i].classList.add('active__tag');
+            theSections[i].classList.add('your-active-class');
         }
-    }   
+    }
 }
 
+
+buildNavItems();
+sectionPositions()
+window.addEventListener('scroll', (event) => {
+    //event.preventDefault()
+    doTheScroll(event)
     
+});
 
 
 
+  
